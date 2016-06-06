e2b6170f9eb359849d7219d313a5774fc80cbf5f
Jong Banaag <aabanaag@gmail.com>
2016-06-06 18:15:40 +0800

    [modified] results checking

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,12 +1,14 @@
 class ResultsController < ApplicationController
   def verify
     results = 0
+    questions = Question.all.to_a
 
     questions_params.each do |e|
+
       id = e['id']
-      answer = e['answer']
-      question = Question.find_by id: id, answer: answer
-      if question
+      answer = e['answer'] || ""
+      question = questions.find { |i| i.id == id  }
+      if question.answer.downcase == answer.downcase
         results += 1
       end
     end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,11 @@
 class Question < ActiveRecord::Base
+  before_save :downcase_fields
+
   validates :question, presence: true, uniqueness: true
   validates :answer, presence: true
+
+  def downcase_fields
+    self.answer.downcase!
+  end
+
 end
